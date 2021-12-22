7ab2d586f85121eaabd7d78bc3692f7cabcd61a8
Laura Bigoni <laurabigoni.92@gmail.com>
2021-12-22 14:40:26 +0100

    stampo correttamente da 1 a 100

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ const gameElement = document.getElementById('game');
 
 let result ='';
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     // Creo un ciclo che stampi in console i numeri da 1 a 100
     result += `${i} `
 }
